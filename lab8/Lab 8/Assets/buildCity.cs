9ea4c0e663a3404ca69be44c9db9f8c431871fa5
Gram Nitschke <gramnitschke@gmail.com>
2020-05-26 13:41:10 -0700

    add a seed

--- lab8/Lab 8/Assets/buildCity.cs
+++ lab8/Lab 8/Assets/buildCity.cs
@@ -11,11 +11,12 @@ public class buildCity : MonoBehaviour
 
     void Start()
     {
+        float seed = Random.Range(0, 100);
         for(int h = 0; h < mapHeight; h++)
         {
             for(int w = 0; w < mapWidth; w++)
             {
-                int result = (int)(Mathf.PerlinNoise(w/10.0f, h/10.0f) * 10);
+                int result = (int)(Mathf.PerlinNoise(w/10.0f + seed, h/10.0f + seed) * 10);
                 Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
                 if(result < 2)
                     Instantiate(buildings[0], pos, Quaternion.identity);
