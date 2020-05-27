56cadf336fb598fe19c80d81a2d49d2ee33e310d
Gram Nitschke <gramnitschke@gmail.com>
2020-05-26 20:08:35 -0700

    floating buildings

--- lab8/Lab 8/Assets/buildCity.cs
+++ lab8/Lab 8/Assets/buildCity.cs
@@ -16,20 +16,36 @@ public class buildCity : MonoBehaviour
         {
             for(int w = 0; w < mapWidth; w++)
             {
-                int result = (int)(Mathf.PerlinNoise(w/10.0f + seed, h/10.0f + seed) * 10);
-                Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
-                if(result < 2)
-                    Instantiate(buildings[0], pos, Quaternion.identity);
-                else if (result < 4)
-                    Instantiate(buildings[1], pos, Quaternion.identity);
-                else if (result < 5)
-                    Instantiate(buildings[2], pos, Quaternion.identity);
-                else if (result < 6)
-                    Instantiate(buildings[3], pos, Quaternion.identity);
-                else if (result < 7)
-                    Instantiate(buildings[4], pos, Quaternion.identity);
-                else if (result < 10)
-                    Instantiate(buildings[5], pos, Quaternion.identity);
+                float y = (int)(Mathf.PerlinNoise(w / 10.0f + seed, h / 10.0f + seed + 20) * 10);
+                if(y < 3)
+                {
+                    y = 0;
+                }
+                else if(y > 8)
+                {
+                    y = 10;
+                }
+                else
+                {
+                    y = -1; 
+                }
+                if (y != -1)
+                {
+                    int result = (int)(Mathf.PerlinNoise(w / 10.0f + seed, h / 10.0f + seed) * 10);
+                    Vector3 pos = new Vector3(w * buildingFootprint, y, h * buildingFootprint);
+                    if (result < 2)
+                        Instantiate(buildings[0], pos, Quaternion.identity);
+                    else if (result < 4)
+                        Instantiate(buildings[1], pos, Quaternion.identity);
+                    else if (result < 5)
+                        Instantiate(buildings[2], pos, Quaternion.identity);
+                    else if (result < 6)
+                        Instantiate(buildings[3], pos, Quaternion.identity);
+                    else if (result < 7)
+                        Instantiate(buildings[4], pos, Quaternion.identity);
+                    else if (result < 10)
+                        Instantiate(buildings[5], pos, Quaternion.identity);
+                }
             }
         }
     }
