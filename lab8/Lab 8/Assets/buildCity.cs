d402c8fb9cc7d3240e4b7dd39ff2413f52cc92e9
Gram Nitschke <gramnitschke@gmail.com>
2020-05-26 20:35:26 -0700

    removal of grass

--- lab8/Lab 8/Assets/buildCity.cs
+++ lab8/Lab 8/Assets/buildCity.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class buildCity : MonoBehaviour
 {
     public GameObject[] buildings;
+    public GameObject cloud;
     public int mapWidth = 200;
     public int mapHeight = 200;
     float buildingFootprint = 1.3f;
@@ -16,19 +17,17 @@ public class buildCity : MonoBehaviour
         {
             for(int w = 0; w < mapWidth; w++)
             {
-                float y = (int)(Mathf.PerlinNoise(w / 10.0f + seed, h / 10.0f + seed + 20) * 10);
-                if(y < 3)
-                {
+                float y = (int)(Mathf.PerlinNoise(w / 10.0f + seed, h / 10.0f + seed + 20) * 30);
+                if(y < 8)
                     y = 0;
-                }
-                else if(y > 8)
-                {
+                else if(y > 13 && y < 17)
                     y = 10;
-                }
+                else if(y > 20 && y < 23)
+                    y = 20;
+                else if(y > 25)
+                    y = 30;
                 else
-                {
-                    y = -1; 
-                }
+                    y = -1;
                 if (y != -1)
                 {
                     int result = (int)(Mathf.PerlinNoise(w / 10.0f + seed, h / 10.0f + seed) * 10);
@@ -37,14 +36,13 @@ public class buildCity : MonoBehaviour
                         Instantiate(buildings[0], pos, Quaternion.identity);
                     else if (result < 4)
                         Instantiate(buildings[1], pos, Quaternion.identity);
-                    else if (result < 5)
-                        Instantiate(buildings[2], pos, Quaternion.identity);
                     else if (result < 6)
+                        Instantiate(buildings[2], pos, Quaternion.identity);
+                    else if (result < 8)
                         Instantiate(buildings[3], pos, Quaternion.identity);
-                    else if (result < 7)
-                        Instantiate(buildings[4], pos, Quaternion.identity);
                     else if (result < 10)
-                        Instantiate(buildings[5], pos, Quaternion.identity);
+                        Instantiate(buildings[4], pos, Quaternion.identity);
+                    Instantiate(cloud, pos, Quaternion.identity);
                 }
             }
         }
